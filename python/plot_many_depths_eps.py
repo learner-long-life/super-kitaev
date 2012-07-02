79aba8c4c8a1c0656fc6ab8a6c4fd87f102009f7
Paul Pham <ppham@local-box.org>
2012-07-02 13:25:26 +0200

    Fixed plotting of amortized size/depth.

--- python/plot_many_depths_eps.py
+++ python/plot_many_depths_eps.py
@@ -7,12 +7,12 @@ fig_width_pt = 346.0  # Get this from LaTeX using \showthe\columnwidth
 inches_per_pt = 1.0/72.27               # Convert pt to inch
 golden_mean = (sqrt(5)-1.0)/2.0         # Aesthetic ratio
 fig_width = fig_width_pt*inches_per_pt  # width in inches
-fig_height = fig_width*golden_mean      # height in inches
+fig_height = fig_width*(golden_mean + 0.1)      # height in inches
 fig_size =  [fig_width,fig_height]
 params = {'backend': 'ps',
           'axes.labelsize': 8,
           'text.fontsize': 10,
-          'legend.fontsize': 10,
+          'legend.fontsize': 8,
           'xtick.labelsize': 8,
           'ytick.labelsize': 8,
           'text.usetex': True,
@@ -49,9 +49,9 @@ def D3(size):
 def D4(size):
 	return numpy.log2(size)
 	
-labels = ['$D=L$', '$D=\sqrt{L}$' ,'$D=\log^2{L}$', '$D=\log{L}$']
+labels = ['$D=L$', '$D=\sqrt{L}$' ,'$D=\log^2{L}$', '$D=\log{L}$', '']
 depth_funcs = [D1, D2, D3, D4]
-shapes = ['-', '.', '*', 'o', '+', ':']
+shapes = ['.', '*', 'o', '+', ':']
 
 for d in range(len(depth_funcs)):
 	for i in range(len(x)):
@@ -62,9 +62,16 @@ for d in range(len(depth_funcs)):
 		n_prime = -numpy.log2(epsilon)
 		resources = cphase(n = n_prime+2, L_prime = L)
 		depth = resources['depth']
-		y[i] = depth / D
+		print "depth["+str(L)+"]=" + str(depth)
+		y[i] = ((D + depth)*1.0) / D
 	
 	pylab.plot(x,y,'g'+shapes[d],label='KSV '+labels[d])
+
+# y = 1 line, for comparison, hypothetical "unit" depth overhead
+for i in range(len(x)):
+	y[i] = 1
+
+pylab.plot(x,y,'b-')
 
 ##############################################################################
 # Plot the baseline plot of Solovay-Kitaev
@@ -93,7 +100,7 @@ y[0] = 1
 axes.set_yscale('log')
 
 pylab.plot(x,y,'rx',label='Solovay-Kitaev')
-pylab.legend(loc='lower right', bbox_to_anchor=(1.0, 0.0),
+pylab.legend(loc='lower right', bbox_to_anchor=(1.0, 0.5),
              ncol=1, fancybox=True, shadow=True)
 
 ##############################################################################
